0bad759bcf67421759f0c77f537ccffbcdf92245
BlackpearlJack <lestercarlyle@gmail.com>
2021-05-03 13:03:58 +0300

    edits on login page

--- login_signup_ui/lib/main.dart
+++ login_signup_ui/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_ui/ui/login_page.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: LoginPage(),
   ));
 }
 
@@ -16,8 +17,11 @@ class HomePage extends StatelessWidget {
             height: size.height,
             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50 ),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Text(
@@ -44,9 +48,61 @@ class HomePage extends StatelessWidget {
                   height: size.height/3,
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('')
+                      image: AssetImage('assets/images/Illustration.png')
                     )
                   ),
+                ),
+                Column(
+                  children: <Widget>[
+                    MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                      ),
+                      child: Text(
+                          "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black)
+                        )
+                      ),
+                      child: MaterialButton(
+                        minWidth: double.infinity,
+                        height: 60,
+                        onPressed: () {},
+                        color: Colors.yellow,
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
